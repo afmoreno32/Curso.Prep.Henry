dc4800c3f998769e120f088de8049f986b325b78
afmoreno32 <asdresito13@gmail.com>
2022-09-14 16:16:22 -0500

    avan numer 3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,30 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+
+    
+    return array[0]
+
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+
+   
+    return array[array.length - 1];
+
+
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+
+  return array.length
+
 }
 
 
@@ -23,6 +35,15 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = array[i] + 1
+      
+    }
+
+  return array;
+
+
 }
 
 
@@ -30,6 +51,11 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+
+  array.push(elemento);
+
+  return array;
+
 }
 
 
@@ -38,6 +64,10 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+
+  array.unshift(elemento);
+  return array;
+
 }
 
 
@@ -47,6 +77,9 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+
+    return palabras.join(" ");
+
 }
 
 
@@ -54,6 +87,14 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+
+
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === elemento){
+        return true;
+    } 
+  }
+  return false;
 }
 
 
@@ -61,6 +102,10 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+
+int = numeros = [5,5,5,5]
+ return numeros.reduce()
+
 }
 
 
